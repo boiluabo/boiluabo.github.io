bbc1690db85cb8620028e8c4e5aed0d91cae1a28
boiluabo <106114769+boiluabo@users.noreply.github.com>
2025-02-05 07:21:08 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -6,3 +6,20 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+}
+
+
+function imges_calc(){
+const a = [
+    {id: 1, name: 'Вася'},
+{id: 2, name: 'Оля'},
+{id: 3, name: 'Петя'},
+]
+
+const od = document.querySelector('div')
+a.forEach(o => {
+    op = document.createElement('p')
+    op.textContent = o.name
+    od.append(op)
+})
+}
