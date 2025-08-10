7c96e257e21dff4c0f3c28613ba30d24d4187e76
boiluabo <106114769+boiluabo@users.noreply.github.com>
2025-08-10 19:29:53 +0300

    Update js.js

--- js.js
+++ js.js
@@ -11,9 +11,9 @@ function myFunction() {
 
 function imges_calc(){
 const a = [
-    {id: 1, name: 'Вася'},
-{id: 2, name: 'Оля'},
-{id: 3, name: 'Петя'},
+    {id: 1, name: '1'},
+{id: 2, name: '2'},
+{id: 3, name: '3'},
 ]
 
 const od = document.querySelector('div')
